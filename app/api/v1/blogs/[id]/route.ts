78f1b27cdb472ee85a36c9a2b06bbdd407574a14
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-23 18:57:19 +0100

    Init the patch logic

--- app/api/v1/blogs/[id]/route.ts
+++ app/api/v1/blogs/[id]/route.ts
@@ -24,6 +24,13 @@ export const PUT = async (req: Request, res: Response) => {
   }
 };
 
+export const PATCH = async (req: Request, res: Response) => {
+  try {
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+};
+
 export const DELETE = async (req: Request, res: Response) => {
   try {
     const id = req.url.split("blogs/")[1];
