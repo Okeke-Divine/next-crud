9cba78bc32cb15bedc781b2db3443d4c7ab115b7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-23 18:08:47 +0100

    Added the get blogs logic

--- app/api/v1/blogs/route.ts
+++ app/api/v1/blogs/route.ts
@@ -1,7 +1,15 @@
+import { NextResponse } from "next/server";
+import { getPosts } from "@/app/lib/data";
+
 export const GET = async (req: Request, res: Response) => {
-    console.log('GET REQUEST');
-}
+    try {
+    const posts = getPosts();
+    return NextResponse.json({ message: "ok", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+};
 
 export const POST = async (req: Request, res: Response) => {
-    console.log('POST REQUEST');
-}
+  console.log("POST REQUEST");
+};
