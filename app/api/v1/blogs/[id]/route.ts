f53bb20954bb8d0d722885a77882cb7333d65da3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-23 18:22:19 +0100

    Added the view sspecific post logic

--- app/api/v1/blogs/[id]/route.ts
+++ app/api/v1/blogs/[id]/route.ts
@@ -1,11 +1,19 @@
+import { getById } from "@/app/lib/data";
+import { NextResponse } from "@/node_modules/next/server";
+
 export const GET = async (req: Request, res: Response) => {
-    console.log('GET');
-}
+  const id = req.url.split("blogs/")[1];
+  const post = getById(id);
+  if (!post) {
+    return NextResponse.json({ message: "Error" }, { status: 400 });
+  }
+  return NextResponse.json({ message: "ok", post }, { status: 200 });
+};
 
 export const PUT = async (req: Request, res: Response) => {
-    console.log('PUT');
-}
+  console.log("PUT");
+};
 
 export const DELETE = async (req: Request, res: Response) => {
-    console.log('DELETE');
-}
+  console.log("DELETE");
+};
