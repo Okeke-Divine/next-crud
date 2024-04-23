3c5c62a8e59a38bbfa1899853b2512cbc747c8d9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-23 17:54:07 +0100

    first commit

---
+++ app/api/v1/blog/[id]/route.ts
@@ -0,0 +1,11 @@
+export const GET = async (req: Request, res: Response) => {
+    console.log('GET');
+}
+
+export const PUT = async (req: Request, res: Response) => {
+    console.log('PUT');
+}
+
+export const DELETE = async (req: Request, res: Response) => {
+    console.log('DELETE');
+}
